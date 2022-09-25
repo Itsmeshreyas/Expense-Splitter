ca02081b108910391fbf6b94a0a317ed8b3715b0
Itsmeshreyas <106860466+Itsmeshreyas@users.noreply.github.com>
2022-09-25 14:25:04 +0530

    abc

--- controllers/SettlementController.java
+++ controllers/SettlementController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.splitexp.dtos.Response;
 import com.splitexp.dtos.UserSettlementDTO;
 import com.splitexp.models.Settlement;
@@ -20,13 +19,11 @@ import com.splitexp.services.GroupExpenseService;
 public class SettlementController {
 	
 	@Autowired private GroupExpenseService expService;
-	
 	@PostMapping
 	public ResponseEntity<?> addGroup(@RequestBody Settlement set){
 		expService.saveSettlementTran(set);
 		return ResponseEntity.ok(new Response("Settlement saved successfully","success"));
 	}
-	
 	@GetMapping
 	public ResponseEntity<?> settlementTrans(UserSettlementDTO dto){
 		return ResponseEntity.ok(expService.findUserSettlement(dto));
@@ -35,6 +32,4 @@ public class SettlementController {
 	@GetMapping("dashboard")
 	public ResponseEntity<?> settlementTransDashboard(UserSettlementDTO dto){
 		return ResponseEntity.ok(expService.findallUserSettlement(dto));
-	}
-
-}
+	}}
